facb867975fe55ddd2ebc2f2538d1e2cc5ca7cf4
Avishka Ishan <avishka@flynow.lk>
2023-10-30 13:55:20 +0530

    Update the code when data is not available its display message called No Data is Available Here...

--- database/seeders/ReIssueSeeder.php
+++ database/seeders/ReIssueSeeder.php
@@ -18,13 +18,27 @@ class ReIssueSeeder extends Seeder
             'airline' => 'Mihin Lanka',
             'total_fare' => '35000.00',
             'airline_pnr' => '545',
-            'pax_name' => 'Pax 2',
+            'pax_name' => 'Pax 1',
             'sector' => 'Sector 4',
             'agency_email' => 'agency2@travelport.lk',
             'admin_email' => 'admin1@travelport.lk',
             'remarks' => 'Remarks New',
-            'no_show' => '4',
-            'purged_pnr' => '8'
+            'no_show' => '1',
+            'purged_pnr' => '0'
+        ]);
+
+        ReIssue::create([
+            'pnr' => 'PNR2',
+            'airline' => 'Sri Lankan',
+            'total_fare' => '35000.00',
+            'airline_pnr' => '545',
+            'pax_name' => 'Pax 2',
+            'sector' => 'Sector 3',
+            'agency_email' => 'agency2@travelport.lk',
+            'admin_email' => 'admin1@travelport.lk',
+            'remarks' => 'Remarks New',
+            'no_show' => '0',
+            'purged_pnr' => '1'
         ]);
     }
 }
--- resources/views/livewire/super-admin/re-isssue.blade.php
+++ resources/views/livewire/super-admin/re-isssue.blade.php
@@ -12,73 +12,84 @@
             </div>
 
             <div class="container">
-            <div class="col-lg-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <h4 class="card-title">Re-issue Data</h4>
-                            </div>
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table class="table table-hover table-responsive-md" cellspacing="20">
-                                        <thead style="width: 300px;">
-                                            <tr>
-                                                <th style="width:80px;"><strong>#</strong></th>
-                                                <th><strong>PNR</strong></th>
-                                                <th><strong>Airline</strong></th>
-                                                <th><strong>Total Fare</strong></th>
-                                                <th><strong>Airline PNR</strong></th>
-                                                <th><strong>Pax Name</strong></th>
-                                                <th><strong>Sector</strong></th>
-                                                <th><strong>Agency Email</strong></th>
-                                                <th><strong>Admin Email</strong></th>
-                                                <th><Strong>Remarks</Strong></th>
-                                                <th><strong>No Show</strong></th>
-                                                <th><strong>Purged PNR</strong></th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Re-issue Data</h4>
+                        </div>
+                        @if ($data->count() > 0)
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-hover table-responsive-md" cellspacing="20">
+                                    <thead style="width: 300px;">
+                                        <tr>
+                                            <th style="width:80px;"><strong>#</strong></th>
+                                            <th><strong>PNR</strong></th>
+                                            <th><strong>Airline</strong></th>
+                                            <th><strong>Total Fare</strong></th>
+                                            <th><strong>Airline PNR</strong></th>
+                                            <th><strong>Pax Name</strong></th>
+                                            <th><strong>Sector</strong></th>
+                                            <th><strong>Agency Email</strong></th>
+                                            <th><strong>Admin Email</strong></th>
+                                            <th><Strong>Remarks</Strong></th>
+                                            <th><strong>No Show</strong></th>
+                                            <th><strong>Purged PNR</strong></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    
                                         @foreach ($data as $item)
-                                            <tr> 
-                                                    <td><strong>{{$item->id}}</strong></td>
-                                                    <td>{{ $item->pnr }}</td>
-                                                    <td>{{ $item->airline }}</td>
-                                                    <td>{{ $item->total_fare }}</td>
-                                                    <td>{{ $item->airline_pnr }}</td>
-                                                    <td>{{ $item->pax_name }}</td>
-                                                    <td>{{ $item->sector }}</td>
-                                                    <td>{{ $item->agency_email }}</td>
-                                                    <td>{{ $item->admin_email }}</td>
-                                                    <td>{{ $item->remarks }}</td>
-                                                    @if ($item->no_show == 1)
-                                                    
-                                                        <td><span class="badge badge-success">Yes</span></td>
-                                                    
-                                                    @elseif ($item->no_show == 0)
-                                                    
-                                                        <td><span class="badge badge-danger">No</span></td>
-                                                    
-                                                    @endif
-                                                    
-                                                    @if ($item->purged_pnr ==1)
+                                        <tr>
+                                            <td><strong>{{$item->id}}</strong></td>
+                                            <td>{{ $item->pnr }}</td>
+                                            <td>{{ $item->airline }}</td>
+                                            <td>RS {{ $item->total_fare }}/=</td>
+                                            <td>{{ $item->airline_pnr }}</td>
+                                            <td>{{ $item->pax_name }}</td>
+                                            <td>{{ $item->sector }}</td>
+                                            <td>{{ $item->agency_email }}</td>
+                                            <td>{{ $item->admin_email }}</td>
+                                            <td>{{ $item->remarks }}</td>
+                                            @if ($item->no_show == 1)
 
-                                                        <td><span class="badge badge-success">True</span></td>
+                                            <td><span class="badge badge-success">Yes</span></td>
 
-                                                    @elseif ($item ->purged_pnr == 0)
-                                                    
-                                                        <td><span class="badge badge-danger">Flase</span></td>
-                                                    @endif
+                                            @elseif ($item->no_show == 0)
 
-                                                
-                                            </tr>
+                                            <td><span class="badge badge-danger">No</span></td>
+
+                                            @endif
+
+                                            @if ($item->purged_pnr ==1)
+
+                                            <td><span class="badge badge-success">True</span></td>
+
+                                            @elseif ($item ->purged_pnr == 0)
+
+                                            <td><span class="badge badge-danger">Flase</span></td>
+                                            @endif
+
+
+                                        </tr>
                                         @endforeach
 
-                                            
-                                        </tbody>
-                                    </table>
-                                </div>
+                                        @else
+
+                                        <figure class="text-center m-6">
+                                            <h3 class="mt-4">No Data is Available Here...</h3>
+                                        </figure>
+                                         
+
+                                    @endif
+
+
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
+                </div>
             </div>
 
 
