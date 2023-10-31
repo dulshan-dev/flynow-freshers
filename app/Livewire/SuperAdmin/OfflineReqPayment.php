65b9d51107f7b85f94440bf4d65fa6cbf6006ae3
AminduW <148184538+AminduW@users.noreply.github.com>
2023-10-31 12:53:57 +0530

    Update OfflineReqPayment.php

--- app/Livewire/SuperAdmin/OfflineReqPayment.php
+++ app/Livewire/SuperAdmin/OfflineReqPayment.php
@@ -5,13 +5,22 @@ namespace App\Livewire\SuperAdmin;
 use Livewire\Component;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
+use App\Models\SubPayment;
+
 
 #[Layout('components.layouts.admin')]
 #[Title('Payments')]
 class OfflineReqPayment extends Component
 {
-    public function render()
-    {
-        return view('livewire.super-admin.offline-req-payment');
-    }
+
+public $ref_no, $amount, $agency_email, $admin_email, $remarks, $document;
+public $payments;
+
+public function render()
+{
+    $this->payments = SubPayment::all();
+
+    return view('livewire.super-admin.offline-req-payment');
+}
+
 }
