6ada004305d0b0138a23dac1c358e909f7403164
sandeepaaaa <97717669+sandeepaaaa@users.noreply.github.com>
2023-11-02 16:28:56 +0530

    admin filtering Non submit sub agents and verifi sub agents also show all users in user tabel

--- app/Livewire/SuperAdmin/ManageSubAgent.php
+++ app/Livewire/SuperAdmin/ManageSubAgent.php
@@ -2,17 +2,47 @@
 
 namespace App\Livewire\SuperAdmin;
 
-use Livewire\Attributes\Layout;
-use Livewire\Attributes\Title;
+use App\Models\User;
 use Livewire\Component;
-
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Layout;
+use App\Models\UserVerification;
 
 #[Layout('components.layouts.admin')]
 #[Title('Manage Sub Agent')]
+
 class ManageSubAgent extends Component
 {
+    public $filter = 'all';
+
+    public function filterUsers()
+    {
+      
+    }
+
+    public function deleteUser($userId)
+    {
+        $user = User::find($userId);
+        
+        if ($user) {
+            $user->delete();
+        }
+    }
+
     public function render()
     {
-        return view('livewire.super-admin.manage-sub-agent');
+        $usersQuery = User::select('users.*', 'user_verifications.status as verification_status')
+            ->join('user_verifications', 'users.id', '=', 'user_verifications.user_id');
+
+        if ($this->filter === 'verified') {
+            $users = $usersQuery->where('user_verifications.status', 'verified')->get();
+        } elseif ($this->filter === 'not_submitted') {
+            $users = $usersQuery->where('user_verifications.status', 'Not Submited')->get();
+        } else {
+            
+            $users = $usersQuery->get();
+        }
+
+        return view('livewire.super-admin.manage-sub-agent', compact('users'));
     }
 }
--- resources/views/livewire/super-admin/manage-sub-agent.blade.php
+++ resources/views/livewire/super-admin/manage-sub-agent.blade.php
@@ -1,116 +1,101 @@
 <div>
-
     <div class="content-body">
         <!-- row -->
         <div class="container-fluid">
-
-
-
-            {{-- sub agent table start --}}
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4 class="card-title">Admin List</h4>
-                            <!-- Large modal -->
-                            <button type="button" class="btn btn-primary mb-2" data-bs-toggle="modal"
-                                data-bs-target=".create-admin-modal">
-                                Create Admin
-                            </button>
-
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table id="example" class="display" style="min-width: 845px">
-                                    <thead>
-                                        <tr>
-                                            <th>
-                                                <div class="form-check custom-checkbox">
-                                                    <input type="checkbox" class="form-check-input" id="checkAll"
-                                                        required="">
-                                                    <label class="form-check-label" for="checkAll"></label>
+            <div class="card">
+                <div class="card-header">
+                    <div class="card-body">
+                        <select wire:model="filter" wire:change='filterUsers' class="form-select form-select-lg mb-3" aria-label="select example">
+                            <option value="all">All Users</option>
+                            <option value="verified">Verified Users</option>
+                            <option value="not_submitted">Pending Users</option>
+                        </select>
+                        <div class="table-responsive">
+                            <table class="table table-sm mb-0 table-striped">
+                                <thead>
+                                    <tr>
+                                        <th class="pe-3">
+                                            <div class="form-check custom-checkbox mx-2">
+                                                <input type="checkbox" class="form-check-input" id="checkAll">
+                                                <label class="form-check-label" for="checkAll"></label>
+                                            </div>
+                                        </th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Company Name</th>
+                                        <th>Status</th>
+                                        <th class="text-center">Email</th>
+                                        <th>Phone</th>
+                                        <th>Role</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="customers">
+                                    @foreach ($users as $user)
+                                    <tr class="btn-reveal-trigger">
+                                        <td class="py-2">
+                                            <div class="form-check custom-checkbox mx-2">
+                                                <input type="checkbox" class="form-check-input" id="checkbox1">
+                                                <label class="form-check-label" for="checkbox1"></label>
+                                            </div>
+                                        </td>
+                                        <td class="py-3">
+                                            <a href="#">
+                                                <div class="media d-flex align-items-center">
+                                                    <div class="avatar avatar-xl me-2">
+                                                        <div class=""><img class="rounded-circle img-fluid" src="{{ $user->profile_photo_path ? asset('storage/' . $user->profile_photo_path) : asset('assets/images/user.jpg') }}" width="30" alt="">
+                                                        </div>
+                                                    </div>
+                                                    <div class="media-body">
+                                                        <h5 class="mb-0 fs--1">{{ $user->first_name }}</h5>
+                                                    </div>
                                                 </div>
-                                            </th>
-                                            <th>First Name</th>
-                                            <th>Last Name</th>
-                                            <th>Company Name</th>
-                                            <th>Email Address</th>
-                                            <th>Phone</th>
-                                            <th>Company Type</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <div class="form-check custom-checkbox ms-2">
-                                                    <input type="checkbox" class="form-check-input" id="customCheckBox2"
-                                                        required="">
-                                                    <label class="form-check-label" for="customCheckBox2"></label>
+                                            </a>
+                                        </td>
+                                        <td class="py-3">
+                                            <a href="#">
+                                                <div class="media d-flex align-items-center">
+                                                    <div class="media-body">
+                                                        <h5 class="mb-0 fs--1">{{ $user->surname }}</h5>
+                                                    </div>
                                                 </div>
-                                            </td>
-                                            <td>Tiger Nixon</td>
-                                            <td>tnixon@example.com</td>
-                                            <td>Accountant</td>
-                                            <td>+94778901212</td>
-                                            <td>Accountant</td>
-                                            <td>+94778901212</td>
-                                            <td>
-                                                <div class="d-flex">
-                                                    <a href="#"
-                                                        class="btn btn-primary shadow btn-xs sharp me-1"><i
-                                                            class="fas fa-pencil-alt"></i></a>
-                                                    <a href="#" class="btn btn-danger shadow btn-xs sharp"><i
-                                                            class="fa fa-trash"></i></a>
+                                            </a>
+                                        </td>
+                                        <td class="py-2">{{ $user->company_name }}</td>
+                                        <td><span class="badge light badge-success">{{ $user->verification_status }}</span></td>
+                                        <td class="py-2 ps-5"><a href="mailto:{{ $user->email }}">{{ $user->email }}</a></td>
+                                        <td class="py-2"><a href="tel:{{ $user->phone }}">{{ $user->phone }}</a></td>
+                                        <td class="py-2">{{ $user->role }}</td>
+                                        <td class="py-2 text-end">
+                                            <div class="dropdown">
+                                                <button class="btn btn-success light sharp" type="button" data-bs-toggle="dropdown">
+                                                    <span class="fs--1">
+                                                        <svg width="20px" height="20px" viewBox="0 0 24 24" version="1.1">
+                                                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                                <rect x="0" y="0" width="24" height="24"></rect>
+                                                                <circle fill="#000000" cx="5" cy="12" r="2"></circle>
+                                                                <circle fill="#000000" cx="12" cy="12" r="2"></circle>
+                                                                <circle fill="#000000" cx="19" cy="12" r="2"></circle>
+                                                            </g>
+                                                        </svg>   
+                                                    </span>
+                                                </button>
+                                                <div class="dropdown-menu dropdown-menu-end border py-0">
+                                                    <div class="py-2">
+                                                        <a class="dropdown-item" href="#" wire:click="editUser({{ $user->id }})">Edit</a>
+                                                        <a class="dropdown-item text-danger" href="#" wire:click="deleteUser({{ $user->id }})">Delete</a>
+                                                    </div>
                                                 </div>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div class="form-check custom-checkbox ms-2">
-                                                    <input type="checkbox" class="form-check-input" id="customCheckBox3"
-                                                        required="">
-                                                    <label class="form-check-label" for="customCheckBox3"></label>
-                                                </div>
-                                            </td>
-                                            <td>Garrett Winters</td>
-                                            <td>Winter@example.org</td>
-                                            <td>Developer</td>
-                                            <td>+91756759863</td>
-                                            <td>Accountant</td>
-                                            <td>+94778901212</td>
-                                            <td>
-                                                <div class="d-flex">
-                                                    <a href="#"
-                                                        class="btn btn-primary shadow btn-xs sharp me-1"><i
-                                                            class="fas fa-pencil-alt"></i></a>
-                                                    <a href="#" class="btn btn-danger shadow btn-xs sharp"><i
-                                                            class="fa fa-trash"></i></a>
-                                                </div>
-                                            </td>
-                                        </tr>
-
-                                    </tbody>
-                                    <!-- <tfoot>
-                                        <tr>
-                                        <th>
-                                                <div class="form-check custom-checkbox ms-2">
-                                                    <input type="checkbox" class="form-check-input" id="checkAll" required="">
-                                                    <label class="form-check-label" for="checkAll"></label>
-                                                </div>
-                                            </th>
-                                            <th>Name</th>
-                                            <th>Email Address</th>
-                                            <th>Country</th>
-                                            <th>Phone</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </tfoot> -->
-                                </table>
-                            </div>
+                                            </div>
+                                        </td>
+                                        
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
             </div>
-            {{-- sub agent table start  ends --}}
+        </div>
+    </div>
 </div>
